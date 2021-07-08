07c5b75bc360ca46e533bcee22b4e1fa412c8c05
estefaneluz <estefanecortes@gmail.com>
2021-07-08 19:58:46 -0300

    add as 3 funçoes no exercicio 03

--- classe-03/base-do-gourmandices/src/App.js
+++ classe-03/base-do-gourmandices/src/App.js
@@ -8,18 +8,32 @@ export const ContextoCarrinhoProduto = createContext();
 
 export default function App() {
   const [carrinho, setCarrinho] = useState({});
-  const incrementarCarrinho = (id, qtd) =>
+  
+  const adicionarCarrinho = (id) => {
     setCarrinho((carrinho) => {
       const novoCarrinho = { ...carrinho };
-      novoCarrinho[id] = (novoCarrinho[id] || 0) + qtd;
+      novoCarrinho[id] = 1;
 
-      if (novoCarrinho[id] === 0) {
-        delete novoCarrinho[id];
-      }
       return novoCarrinho;
-    });
+  })}
 
-  const adicionarCarrinho=(id) => incrementarCarrinho(id, 1); 
+  const mudarQtdNoCarrinho = (id, qtd) =>
+    setCarrinho((carrinho) => {
+      const novoCarrinho = { ...carrinho };
+      novoCarrinho[id] += qtd;
+
+      if(novoCarrinho[id] === 0) removerDoCarrinho(id);
+
+      return novoCarrinho;
+  });
+
+  const removerDoCarrinho = (id) => {
+    setCarrinho((carrinho) => {
+      const novoCarrinho = { ...carrinho };
+      delete novoCarrinho[id];
+      return novoCarrinho;
+    })
+  }
 
   const valueContext = {carrinho, produtos, incrementarCarrinho, adicionarCarrinho}
   return (
